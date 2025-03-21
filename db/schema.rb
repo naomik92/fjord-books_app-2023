d4b7f80c23561de30bb361364af500059e0410ce
naomik92 <161175746+naomik92@users.noreply.github.com>
2025-03-21 13:24:13 +0900

    usersテーブルにカラムを追加する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_03_20_123358) do
+ActiveRecord::Schema[7.0].define(version: 2025_03_21_042026) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -28,6 +28,9 @@ ActiveRecord::Schema[7.0].define(version: 2025_03_20_123358) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "postcode"
+    t.string "address"
+    t.text "self_introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

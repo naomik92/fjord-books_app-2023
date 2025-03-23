c8aace92e00f19da6601042c1534ec0f1b0abd05
naomik92 <161175746+naomik92@users.noreply.github.com>
2025-03-23 19:07:41 +0900

    本の一覧ページにUser一覧ページへのリンクを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/:id', to: 'users#show'
   devise_for :users
+  get 'users/index'
+  get 'users/:id', to: 'users#show', as: 'user'
   resources :books
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

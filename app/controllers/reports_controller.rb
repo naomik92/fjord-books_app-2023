a6df270220ea4e4b54bb3be9716b4097e685255a
naomik92 <161175746+naomik92@users.noreply.github.com>
2025-05-17 10:40:11 +0900

    ReportのViewを作成、投稿日時の表示

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,13 +1,72 @@
+# frozen_string_literal: true
+
 class ReportsController < ApplicationController
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
   def index
+    @reports = Report.order(:id).page(params[:page])
   end
 
   def show
+    @report = Report.find(params[:id])
   end
 
+  # GET /reports/new
   def new
+    @report = Report.new
   end
 
-  def edit
+  # GET /reports/1/edit
+  def edit; end
+
+  # POST /reports
+  def create
+    @report = Report.new(report_params)
+
+    respond_to do |format|
+      if @report.save
+        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human) }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
   end
+
+  # PATCH/PUT /reports/1
+  def update
+    respond_to do |format|
+      if @report.update(report_params)
+        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human) }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /reports/1
+  def destroy
+    @report.destroy
+
+    respond_to do |format|
+      format.html { redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human) }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_report
+    @report = Report.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def report_params
+    params.require(:report).permit(:title, :description)
+  end
+
+  def correct_user
+    @user = User.find(51)
+    redirect_to reports_path unless @user == current_user
+  end
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class Book < ApplicationRecord
+  has_many :reports
   mount_uploader :picture, PictureUploader
 end

74ad347cf95e6475507ed1524465ac2fcc7d2a70
naomik92 <161175746+naomik92@users.noreply.github.com>
2025-06-23 07:18:14 +0900

    application_controller.rbを微修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-
   protected
 
   def configure_permitted_parameters

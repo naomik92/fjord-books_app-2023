486f06ad051906dd9b46d77d069daff025f7e221
naomik92 <161175746+naomik92@users.noreply.github.com>
2025-08-08 09:23:08 +0900

    userモデル、reportモデルのテストを作成

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,14 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#mentioning_ids' do
+    report = Report.create(title: '日報テスト', content: '日報を参照 http://localhost:3000/reports/1')
+    assert_equal ['1'], report.mentioning_ids(report.content)
+
+    report.content = '日報を参照 http://localhost:3000/reports/1http://localhost:3000/reports/2http://localhost:3000/reports/3'
+    assert_equal ['1', '2', '3'], report.mentioning_ids(report.content)
+
+    report.content = '日報の参照なし'
+    assert_equal [], report.mentioning_ids(report.content)
+  end
 end

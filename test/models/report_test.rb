84328af6a600c20d7f65a9b756902ce2bacf447b
naomik92 <161175746+naomik92@users.noreply.github.com>
2025-08-10 18:43:41 +0900

    test/models/report_test.rbを微修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -14,40 +14,40 @@ class ReportTest < ActiveSupport::TestCase
 
   test '#created_on' do
     report = reports(:report)
-    assert_equal Date.today, report.created_on
+    assert_equal Time.zone.today, report.created_on
   end
 
   test '#create_mention' do
-    report = reports(:report_02)
+    report = reports(:report02)
     ids = ['70219655']
     assert report.create_mention(ids)
   end
 
   test '#destroy_mention' do
-    report = reports(:report_02)
+    report = reports(:report02)
     ids = ['70219655']
     assert report.destroy_mention(ids)
   end
 
   test '#mentioning_ids' do
-    report = reports(:report_02)
+    report = reports(:report02)
     assert_equal ['70219655'], report.mentioning_ids(report.content)
 
     report.content = '日報を参照 http://localhost:3000/reports/1http://localhost:3000/reports/2http://localhost:3000/reports/3'
-    assert_equal ['1', '2', '3'], report.mentioning_ids(report.content)
+    assert_equal %w[1 2 3], report.mentioning_ids(report.content)
 
     report.content = '日報の参照なし'
     assert_equal [], report.mentioning_ids(report.content)
   end
 
   test '#update_mentions' do
-    report = reports(:report_02)
+    report = reports(:report02)
     report.content = '日報を参照 http://localhost:3000/reports/363103885'
     assert report.update_mentions(report.content)
   end
 
   test '#save_report_and_mentions' do
-    report = reports(:report_02)
+    report = reports(:report02)
     assert report.save_report_and_mentions(report.content)
   end
 end

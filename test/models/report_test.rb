32eea0fcda99c21adfc4e761fbe36c155c9e2644
naomik92 <161175746+naomik92@users.noreply.github.com>
2025-08-27 10:34:29 +0900

    reportモデルのテストを書く

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,17 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:alice)
+  end
+
+  test '#editable?' do
+    report = reports(:report)
+    assert report.editable?(@user)
+  end
+
+  test '#created_on' do
+    report = reports(:report)
+    assert_equal Time.zone.today, report.created_on
+  end
 end

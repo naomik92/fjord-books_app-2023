14b1cbd1844a0b9e2245e8e98b80fd1b5dc2cc9a
naomik92 <161175746+naomik92@users.noreply.github.com>
2025-09-06 14:32:53 +0900

    Reportモデルのテストを修正する

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -8,18 +8,33 @@ class ReportTest < ActiveSupport::TestCase
   end
 
   test '#editable?' do
-    report = reports(:report)
-    assert report.editable?(@user)
+    report_01 = Report.new(user_id: 663665735)
+    assert report_01.editable?(@user)
+
+    report_02 = Report.new(user_id: 100000000)
+    refute report_02.editable?(@user)
   end
 
   test '#created_on' do
-    report = reports(:report)
-    assert_equal Time.zone.today, report.created_on
+    assert_equal Time.zone.today, reports(:report01).created_on
   end
 
   test '#save_mentions' do
-    report = reports(:report02)
-    report.save
-    assert_equal [reports(:report)], report.mentioning_reports.to_a
+    report_11 = Report.new(user_id: 902541635, title: 'タイトル', content: '日報の言及なし')
+    report_11.save
+    assert_equal [], report_11.mentioning_reports.to_a
+
+    report_12 = Report.new(user_id: 902541635, title: 'タイトル', content: 'http://localhost:3000/reports/516905418')
+    report_12.save
+    assert_equal [reports(:report01)], report_12.mentioning_reports.to_a
+
+    report_11.update(title: 'タイトル', content: 'http://localhost:3000/reports/516905418')
+    assert_equal [reports(:report01)], report_11.mentioning_reports.to_a
+
+    report_12.update(title: 'タイトル', content: '日報の言及なし')
+    assert_equal [], report_12.reload.mentioning_reports.to_a
+
+    report_11.destroy
+    refute_includes reports(:report01).mentioned_reports, report_11
   end
 end

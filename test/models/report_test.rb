a81c0d0cf6809c74e87ba584f68a1e9f9037ad54
naomik92 <161175746+naomik92@users.noreply.github.com>
2025-09-10 09:08:14 +0900

    Reportモデルのcreated_onとsave_mentionsのテスト修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -16,26 +16,30 @@ class ReportTest < ActiveSupport::TestCase
   end
 
   test '#created_on' do
-    assert_equal Time.zone.today, reports(:report01).created_on
+    travel_to Time.zone.local(2025, 1, 1, 1, 1, 1) do
+      report3 = Report.create(user_id: @user.id, title: 'タイトル', content: '日報の内容')
+      assert_equal Date.new(2025, 1, 1), report3.created_on
+    end
   end
 
   test '#save_mentions' do
-    report10 = Report.new(user_id: @user.id, title: 'タイトル', content: '言及される日報')
-    report10.save
+    report10 = Report.create(user_id: @user.id, title: 'タイトル', content: '言及される日報')
 
-    report11 = Report.new(user_id: @user.id, title: 'タイトル', content: '日報の言及なし')
-    report11.save
-    assert_equal [], report11.mentioning_reports.to_a
+    report11 = Report.create(user_id: @user.id, title: 'タイトル', content: '日報の言及なし')
+    assert_not_includes report11.mentioning_reports, report10
+    assert_not_includes report10.mentioned_reports, report11
 
-    report12 = Report.new(user_id: @user.id, title: 'タイトル', content: "http://localhost:3000/reports/#{report10.id}")
-    report12.save
-    assert_equal [report10], report12.mentioning_reports.to_a
+    report12 = Report.create(user_id: @user.id, title: 'タイトル', content: "http://localhost:3000/reports/#{report10.id}")
+    assert_includes report12.mentioning_reports, report10
+    assert_includes report10.mentioned_reports, report12
 
     report11.update(title: 'タイトル', content: "http://localhost:3000/reports/#{report10.id}")
-    assert_equal [report10], report11.mentioning_reports.to_a
+    assert_includes report11.mentioning_reports, report10
+    assert_includes report10.mentioned_reports, report11
 
     report12.update(title: 'タイトル', content: '日報の言及なし')
-    assert_equal [], report12.reload.mentioning_reports.to_a
+    assert_not_includes report12.reload.mentioning_reports, report10
+    assert_not_includes report10.mentioned_reports, report12
 
     report11.destroy
     assert_not_includes report10.mentioned_reports, report11
